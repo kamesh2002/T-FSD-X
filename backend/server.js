04a0b6dcc4a0a26c39b745f95152339779e69068
“ <727721eucb019@skcet.ac.in>
2025-01-30 22:14:18 +0530

    second

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 const PORT = process.env.PORT || 5039;
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: "5mb" })); // to parse req.body
+app.use(express.json({ limit: "5mb" })); //  req.body
 // limit shouldn't be too high to prevent DOS
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
